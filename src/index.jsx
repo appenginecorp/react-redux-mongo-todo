5d0ead01b6d897571900eeae1f9567692d01ca91
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-14 16:54:47 -0300

    .editorconfig

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom';
 import Field from "./component";
 
 ReactDOM.render(
-    <Field initialValue="Teste1"/>,
-    document.getElementById('app')
+  <Field initialValue="Teste1"/>,
+  document.getElementById('app')
 );
