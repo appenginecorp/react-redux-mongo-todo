fed9aef1041f65a2d416eae56c302aa789fc1e6e
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-15 14:26:56 -0300

    grid

--- app/src/app/todo/todo.jsx
+++ app/src/app/todo/todo.jsx
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 
 import PageHeader from '../template/header';
+import TodoForm from './todo.form';
 
 export default class Todo extends Component {
   render() {
     return (
       <div>
         <PageHeader name="Tarefas" small="Cadastro"></PageHeader>
+        <TodoForm/>
       </div>
     )
   }
