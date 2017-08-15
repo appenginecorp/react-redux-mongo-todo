8839d00fc48780450f4d677876b400d4f62d3cf9
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-15 09:26:11 -0300

    cors

--- api/src/api/todo.service.js
+++ api/src/api/todo.service.js
@@ -1,6 +1,6 @@
 const todoSchema = require('./todo.schema');
 
-todoSchema.methods(['get', 'post', 'put', 'delte']);
+todoSchema.methods(['get', 'post', 'put', 'delete']);
 todoSchema.updateOptions({new: true, runValidators: true});
 
 module.exports = todoSchema;
