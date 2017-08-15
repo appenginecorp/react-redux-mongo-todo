83f76c818e0ee22552efb6669ef51d0467b879b9
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-15 16:12:50 -0300

    list, remove, done, undo

--- app/src/app/todo/todo.jsx
+++ app/src/app/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import PageHeader from '../template/header';
 import TodoForm from './todo.form';
+import TodoList from './todo.list';
+
+const API_URL = 'http://localhost:3003/api/todos';
 
 export default class Todo extends Component {
   constructor(props) {
@@ -9,14 +13,45 @@ export default class Todo extends Component {
     this.state = {description: '', list: []};
     this.handleAdd = this.handleAdd.bind(this);
     this.handleChange = this.handleChange.bind(this);
+    this.handleRemove = this.handleRemove.bind(this);
+    this.handleMarkAsDone = this.handleMarkAsDone.bind(this);
+    this.handleMarkAsPending = this.handleMarkAsPending.bind(this);
+    this.refresh();
+  }
+
+  refresh() {
+    axios.get(`${API_URL}?sort=-createdAt`).then((response) => {
+      this.setState({...this.state, description: '', list: response.data});
+    });
   }
 
   handleAdd() {
-    console.log(this.state.description);
+    const description = this.state.description;
+    axios.post(API_URL, {description}).then(() => {
+      this.refresh();
+    });
   }
 
   handleChange(event) {
     this.setState({...this.state, description: event.target.value});
+  }
+
+  handleRemove(todo) {
+    axios.delete(`${API_URL}/${todo._id}`).then(() => {
+      this.refresh();
+    });
+  }
+
+  handleMarkAsDone(todo) {
+    axios.put(`${API_URL}/${todo._id}`, {...todo, done: true}).then(() => {
+      this.refresh();
+    });
+  }
+
+  handleMarkAsPending(todo) {
+    axios.put(`${API_URL}/${todo._id}`, {...todo, done: false}).then(() => {
+      this.refresh();
+    });
   }
 
   render() {
@@ -27,6 +62,11 @@ export default class Todo extends Component {
           description={this.state.description}
           handleChange={this.handleChange}
           handleAdd={this.handleAdd}/>
+        <TodoList
+          list={this.state.list}
+          handleRemove={this.handleRemove}
+          handleMarkAsDone={this.handleMarkAsDone}
+          handleMarkAsPending={this.handleMarkAsPending}/>
       </div>
     )
   }
---
+++ app/src/app/todo/todo.list.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import IconButton from '../template/icon.btn';
+
+export default props => {
+
+  const renderRows = () => {
+    const list = props.list || [];
+    return list.map(todo => (
+      <tr key={todo._id}>
+        <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
+        <IconButton style="success" icon="check" hide={todo.done} onClick={() => props.handleMarkAsDone(todo)}/>
+        <IconButton style="warning" icon="undo" hide={!todo.done} onClick={() => props.handleMarkAsPending(todo)}/>
+        <IconButton style="danger" icon="trash-o" hide={!todo.done} onClick={() => props.handleRemove(todo)}/>
+      </tr>
+    ));
+  };
+
+  return (
+    <table className="table">
+      <thead>
+      <tr>
+        <th>Descrição</th>
+        <th>Ações</th>
+      </tr>
+      </thead>
+      <tbody>
+      {renderRows()}
+      </tbody>
+    </table>
+  )
+}
