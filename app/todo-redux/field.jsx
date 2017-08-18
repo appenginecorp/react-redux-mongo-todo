ada4e9a814c157df30629b294dd6e307fcb3871d
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-18 11:11:59 -0300

    redux example wip

--- app/todo-redux/field.jsx
+++ app/todo-redux/field.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {connect} from 'react-redux';
 
-export default props => (
-  <h1>{props.initialValue}</h1>
-)
+class Field extends Component {
+  render() {
+    return (
+      <div>
+        <label>{this.props.value}</label><br/>
+        <input onChange={this.handleChange} value={this.props.value}/>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state) {
+  return {
+    value: state.field.value
+  }
+}
+
+export default connect(mapStateToProps)(Field);
