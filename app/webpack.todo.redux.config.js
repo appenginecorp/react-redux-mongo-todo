7a251bf1cc0b0d9cb76c270523c5f8e2f4f93f46
Neri Bez Fontana <nerifontana@gmail.com>
2017-08-18 10:59:14 -0300

    redux basic

--- app/webpack.todo.redux.config.js
+++ app/webpack.todo.redux.config.js
@@ -1,1 +1,40 @@
-//.
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: './todo-redux/index.jsx',
+  output: {
+    path: __dirname + 'todo-redux/public',
+    filename: './app.js'
+  },
+  devServer: {
+    port: 4200,
+    contentBase: './todo-redux/public'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      modules: __dirname + '/node_modules'
+    }
+  },
+  plugins: [
+    new ExtractTextPlugin('app.css')
+  ],
+  module: {
+    loaders: [{
+      test: /.js[x]?$/,
+      loader: 'babel-loader',
+      exclude: '/node_modules/',
+      query: {
+        presets: ['es2015', 'react'],
+        plugins: ['transform-object-rest-spread']
+      }
+    }, {
+      test: /.css$/,
+      loader: ExtractTextPlugin.extract('css-loader', 'style-loader')
+    }, {
+      test: /\.woff|.woff2|.ttf|.eot|.svg*.*$/,
+      loader: 'file-loader'
+    }]
+  }
+};
